3a53f7bb7f8a82b2af6bc097d2cf1d8503d24515
Thanosboss1 <thanosboss01@gmail.com>
2024-07-29 17:06:37 +0800

    Update recover_vless.py

--- vless/recover_vless.py
+++ vless/recover_vless.py
@@ -25,12 +25,6 @@ accounts_json = os.getenv('ACCOUNTS_JSON')
 telegram_token = os.getenv('TELEGRAM_TOKEN')
 telegram_chat_id = os.getenv('TELEGRAM_CHAT_ID')
 
-if not accounts_json or not telegram_token or not telegram_chat_id:
-    error_message = "环境变量缺失"
-    print(error_message)
-    send_telegram_message(telegram_token, telegram_chat_id, error_message)
-    exit(1)
-
 # 检查并解析 JSON 字符串
 try:
     servers = json.loads(accounts_json)
@@ -49,19 +43,22 @@ default_restore_command = "~/.npm-global/bin/pm2 resurrect"
 
 # 遍历服务器列表并执行恢复操作
 for server in servers:
+    host = server['host']
+    port = server['port']
+    username = server['username']
+    password = server['password']
+    cron_command = server.get('cron', default_restore_command)
+
+    print(f"连接到 {host}...")
+
+    # 执行恢复命令（这里假设使用 SSH 连接和密码认证）
+    restore_command = f"sshpass -p '{password}' ssh -o StrictHostKeyChecking=no -p {port} {username}@{host} '{cron_command}'"
     try:
-        host = server['host']
-        port = server['port']
-        username = server['username']
-        password = server['password']
-        cron_command = server.get('cron', default_restore_command)
-
-        print(f"连接到 {host}...")
-
-        # 执行恢复命令（这里假设使用 SSH 连接和密码认证）
-        restore_command = f"sshpass -p '{password}' ssh -o StrictHostKeyChecking=no -p {port} {username}@{host} '{cron_command}'"
         subprocess.check_output(restore_command, shell=True, stderr=subprocess.STDOUT)
         success_count += 1
+    except subprocess.CalledProcessError as e:
+        failed_servers.append(host)
+        print(f"无法恢复 {host} 上的 vless 服务：{e.output.decode('utf-8')}")
     except Exception as e:
         failed_servers.append(host)
         print(f"无法恢复 {host} 上的 vless 服务：{str(e)}")
